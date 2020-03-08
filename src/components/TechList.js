42ad2e1ec6d2901787832b408e2da1c0f4ee65f0
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-03-08 20:09:21 -0300

    feat(state): Handling state and props

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,21 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import TechItem from "../components/TechItem";
 
 class TechList extends Component {
   state = {
-    techList: [
-      'react',
-      'react native',
-      'nodejs'
-    ]
+    newTech: "",
+    techList: ["react", "react native", "nodejs"]
+  };
+
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({
+      techList: [...this.state.techList, this.state.newTech],
+      newTech: ""
+    });
+  };
+
+  handleTechItemDelete = tech => {
+    const newTechList = this.state.techList.filter(t => t !== tech);
+
+    this.setState({ techList: [...newTechList] });
   };
 
   render() {
     return (
-      <ul>
-        <li>React</li>
-        <li>React Native</li>
-        <li>Nodejs</li>
-      </ul>
+      <>
+        <form onSubmit={this.handleSubmit}>
+          <ul>
+            {this.state.techList &&
+              this.state.techList.map(tech => (
+                <TechItem
+                  key={tech}
+                  tech={tech}
+                  onDelete={() => this.handleTechItemDelete(tech)}
+                />
+              ))}
+          </ul>
+
+          <input
+            type="text"
+            onChange={this.handleInputChange}
+            value={this.newTech}
+          />
+          <button type="submit">Add</button>
+        </form>
+      </>
     );
   }
 }
