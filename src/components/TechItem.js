d9ab99e3fb21e0fb198c11e700c0a06029dad918
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-03-08 20:16:12 -0300

    feat(props): Add prop-types for prop validation

--- src/components/TechItem.js
+++ src/components/TechItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 function TechItem({ tech, onDelete }) {
   return (
@@ -10,5 +11,10 @@ function TechItem({ tech, onDelete }) {
     </li>
   );
 }
+
+TechItem.propTypes = {
+  tech: PropTypes.string.isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
 
 export default TechItem;
