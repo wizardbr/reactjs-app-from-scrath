a03a12271190fb1e4ddc52bef58cc3f5a26fdd30
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-03-08 16:03:54 -0300

    feat(webpack): Add style loader on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-const path = require('path');
+const path = require("path");
 
-module.exports ={ 
-  entry: path.resolve(__dirname, 'src', 'index.js'),
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.js"),
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'public')
+    contentBase: path.resolve(__dirname, "public")
   },
   module: {
     rules: [
@@ -15,8 +15,12 @@ module.exports ={
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: "babel-loader"
         }
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
       }
     ]
   }
